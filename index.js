43cf449e646f6173b58927825de25ae13eff9ecd
Rachel Tan <tanrachel823@gmail.com>
2019-11-14 00:24:43 +0000

    alter file location

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var express = require("express"),
  bodyParser = require("body-parser");
 // using dependencies-----------------------------------------------
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+// app.use(express.static("public"));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+// app.set("view engine", "ejs");
 // routes-----------------------------------------------------------
 app.get("/",function(req,res){
-	res.render("home");
-}) 
+	res.render("./static/home/home.html");
+}) ;
 
 
 // listening--------------------------------------------------------
